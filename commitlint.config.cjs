02e6a7584adcafec63d6f4383c950e771bcb0402
richardmoalic <moalicrichard@protonmail.com>
2026-03-31 09:34:04 +0200

    feat(components): a test for components
    Overview:
    loremmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
    
    Changes:
    loremmmmmmmmmmmmmmmmm
    
    Footer:
    
    loremmmmmmmmmmmmmmm

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,44 +1,11 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    prompt: {
-        useEmoji: false,
-        // REMOVED "breaking" and "footer" from this list
-        skipQuestions: ["scope", "footerPrefix"],
-
-        // 1. Define the custom labels
-        messages: {
-            type: "Select type:",
-            subject: "Short description:",
-            body: "Overview (high-level):",
-            // breaking will now ask for Features
-            breaking: "List the Features (use '-' for bullets):",
-
-            footer: "3. Structure (Files added/modified):",
-
-            issues: "4. Responsibilities (Who/What does this affect?):",
-            confirmCommit: "Confirm commit?",
-        },
-
-        isIssueAffected: true,
-
-        formatMessageCB: ({ type, subject, body, breaking, footer, issues }) => {
-            return `${type}: ${subject}
-Overview:
-${body || "N/A"}
-
-Features:
-${breaking || "- N/A"}
-
-Structure:
-${footer || "- N/A"}
-
-Responsibilities:
-${issues || "- N/A"}`;
-        },
-    },
-    rules: {
-        "type-enum": [2, "always", ["feat", "fix", "refactor", "test", "chore"]],
-        "subject-case": [0],
-        "body-leading-blank": [1, "always"],
-    },
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "refactor", "perf", "docs", "chore"]],
+    "subject-case": [0],
+    "body-leading-blank": [2, "always"],
+    "header-max-length": [0], 
+    "body-max-line-length": [0],
+    "footer-max-line-length": [0],
+  },
 };
