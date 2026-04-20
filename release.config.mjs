99693aadb1935833d70c5ef31d040163e671911e
richardmoalic <moalicrichard@protonmail.com>
2026-04-20 04:36:33 +0200

    feat(components): merge build + deploy in one workflow
    Overview:
    N/A
    
    Changes:
    N/A
    
    Footer:
    N/A

--- release.config.mjs
+++ release.config.mjs
@@ -89,13 +89,6 @@ export default {
       },
     ],
 
-    // Update CHANGELOG.md
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
 
     // Create GitHub Release
     [
@@ -105,12 +98,5 @@ export default {
       },
     ],
 
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
   ],
 };
